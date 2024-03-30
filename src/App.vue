1d077dbb941a19060fae4703d9101efa7fd02226
咸鱼仙君 <1796792003@qq.com>
2024-03-30 16:22:55 +0800

    重构Login组件为组合式Api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <Login/>
+  <Login></Login>
 </template>
 
 <script>
-import Login from './components/Login.vue'
+import LO from "@/components/Login.vue";
+import Login from "@/components/Login.vue";
 export default {
   components:{
-    Login
+    Login,
   }
 }
 </script>
